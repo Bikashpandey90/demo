9dcd2363382e114df953636e7420bf1583f15d17
Bikashpandey90 <bikashpandey835@gmail.com>
2025-12-01 15:17:49 +0545

    Fixes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const app = require("./src/config/express.config");
+const port = process.env.PORT || 9005;
 
 const httpServer = http.createServer(app)
 
-const port = process.env.PORT || 9005;
-httpServer.listen((err) => {
+httpServer.listen(port, (err) => {
     if (!err) {
         console.log(`Server is running on port ${port}`);
         console.log("Press CTRL+C to stop it");
