c67a1c3e00850812e55c63baa24982379f2d34ae
Bikashpandey90 <bikashpandey835@gmail.com>
2025-12-01 13:52:42 +0545

    Build Fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const httpServer = http.createServer(app)
 // const port = process.env.PORT || 9005;
 httpServer.listen((err) => {
     if (!err) {
-        console.log(`Server is running on port ${port}`);
+        // console.log(`Server is running on port ${port}`);
         console.log("Press CTRL+C to stop it");
 
     }
